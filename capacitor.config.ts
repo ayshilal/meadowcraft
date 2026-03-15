260f75dc9d0f1e501ee5a62e0a71d9466ac191c4
Hilal <hilal@Hilals-MacBook-Air.local>
2026-03-14 22:37:51 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'elegant-lux-selfcare',
+  webDir: 'www'
+};
+
+export default config;
